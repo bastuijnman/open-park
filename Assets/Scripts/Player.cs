814afdd5d0f46f18178502cf3cd6285e40373f29
Bas Tuijnman <bas.tuijnman@24i.com>
2016-05-05 14:47:26 +0200

    Added basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Movement speed, can be set in the editor
+	public float speed = 1.0f;
+
+	// Player (camera) rotation
+	private Vector3 playerRotation = new Vector3 (35, 0, 0);
+
+	// Player (camera) height
+	private float playerHeight = 5.0f;
+
+	void Start () {
+
+		// Setup the player camera
+		Camera camera = gameObject.AddComponent<Camera> ();
+	}
+		
+	void Update () {
+
+		float horizontalMovement = Input.GetAxis ("Horizontal");
+		float verticalMovement = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (horizontalMovement, 0.0f, verticalMovement);
+
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		// Keep position with a variable height
+		transform.position = new Vector3 (transform.position.x, playerHeight, transform.position.z);
+
+		// Set player rotation
+		transform.rotation = Quaternion.Euler (playerRotation);
+	
+	}
+}
